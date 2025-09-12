e2fccae02163f71b17e3eafd4e1bf8e215959cdf
NicolasCh25 <nicolas.chiguano@epn.edu.ec>
2025-09-12 12:12:07 -0500

    Nombre

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-<<<<<<< HEAD
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Nicolás Chiguno");
-=======
+
 using System;
 
 class Program
@@ -10,6 +7,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hola Mundo");
+        Console.WriteLine("Nicolás Chiguano");
     }
 }
->>>>>>> 81507a7b317d71b2b343ad72b5443c290b09d38c
+
