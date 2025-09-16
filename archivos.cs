30ea6e2072795d1394c72f018534a08425ecaa77
NicolasCh25 <nicolas.chiguano@epn.edu.ec>
2025-09-16 12:53:42 -0500

    Archivos

--- archivos.cs
+++ archivos.cs
@@ -1,16 +1,25 @@
 using System;
+using System.Collections;
 using System.IO;
 
 public class archivos
 {
     public static void Ejecutar()
     {
-        Console.WriteLine("Nombre: ");
-        string nombre = Console.ReadLine();
-        Console.WriteLine("Apellido: ");
-        string apellido = Console.ReadLine();
-        Console.WriteLine("Edad: ");
-        int edad = int.Parse(Console.ReadLine());
-        File.WriteAllText("Datos.txt", $"Nombre: {nombre}\n"+ $"Apellido:{apellido}\n"+$"Edad: {edad}");
+        File.WriteAllText("Datos.txt", string.Empty);
+        Console.WriteLine("Ingrese la cantidad de estudiantes a registrar: ");
+        int cantidad = int.Parse(Console.ReadLine());
+        File.AppendAllText("Datos.txt",$"Item  |"+ $"Nombre    |" + $"Apellido     |" + $"Edad     \n");
+        for (int i = 0; i < cantidad; i++)
+        {
+            Console.WriteLine("Nombre: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Apellido: ");
+            string apellido = Console.ReadLine();
+            Console.WriteLine("Edad: ");
+            int edad = int.Parse(Console.ReadLine());
+            File.AppendAllText("Datos.txt",$"{i+1}       "+$"{nombre}    "+$"{apellido}      "+$"{edad}     \n");
+            
+        }
     }
 }
